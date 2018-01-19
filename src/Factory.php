978ccb4a6167d1e4adbf847302f3991ba4cf71e6
Rasmus Bengtsson <git@rasmusbengtsson.se>
2018-01-19 16:31:08 +0100

    Fix phpdoc and PSR-2 stylefix

--- src/Factory.php
+++ src/Factory.php
@@ -1,8 +1,11 @@
-<?php namespace Maer\Config;
+<?php
+
+namespace Maer\Config;
+
 /**
- * A simple config package to load files containing multidimensional arrays 
+ * A simple config package to load files containing multidimensional arrays
  * and fetch them easily using dot notation.
- * 
+ *
  * @author     Magnus Eriksson <mange@reloop.se>
  * @version    1.1.0
  * @package    Maer
@@ -12,24 +15,26 @@
 /**
  * Factory to get the same Config instance
  */
-class Factory 
+class Factory
 {
     /**
      * The Config instance
-     * @var \Maer\Config\Config
+     * @var Config
      */
     protected static $instance;
 
     /**
      * This class shouldn't be instanceable. It's a static helper class.
      */
-    protected function __construct(){}
+    protected function __construct()
+    {
+    }
 
 
     /**
      * Get the Config instance
-     * 
-     * @return \Maer\Config\Config
+     *
+     * @return Config
      */
     public static function getInstance()
     {
